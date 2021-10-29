b180f4307e4fb560a397862ad32fa01196f5fb7a
EhWanu <cameronjshadgett@gmail.com>
2021-10-29 17:26:39 -0400

    user post with password hash

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,10 @@ router.post("/register", async (req, res) => {
 	const newUser = new User({
 		username: req.body.username,
 		email: req.body.email,
-		password: req.body.password,
+		password: CryptoJs.AES.encrypt(
+			req.body.password,
+			process.env.SECRET_KEY
+		).toString(),
 	});
 	try {
 		const user = await newUser.save();
