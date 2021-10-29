eee0226529faa5b181e9303a8d16c03f2e850b2e
EhWanu <cameronjshadgett@gmail.com>
2021-10-29 14:37:04 -0400

    created models folder and files

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() =>
+		console.log("connection successful!")
+	)
+	.catch((err) => console.log(err));
 
 app.listen(8000, () => {
 	console.log("Backend is up");
