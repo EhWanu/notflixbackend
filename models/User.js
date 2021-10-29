cc6bcc8b451045f1761803fccc6d28cc46fb74ac
EhWanu <cameronjshadgett@gmail.com>
2021-10-29 14:41:03 -0400

    created schemas

--- models/User.js
+++ models/User.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema();
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	ProfilePic: { type: String, default: "" },
+	isAdmin: { type: boolean, default: "false" },
+});
