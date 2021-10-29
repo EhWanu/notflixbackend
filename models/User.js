91a1cb8be57551e052ed26f6c693e2f556444d9d
EhWanu <cameronjshadgett@gmail.com>
2021-10-29 16:12:30 -0400

    schema for  movies

--- models/User.js
+++ models/User.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		ProfilePic: { type: String, default: "" },
+		isAdmin: { type: boolean, default: "false" },
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	ProfilePic: { type: String, default: "" },
-	isAdmin: { type: boolean, default: "false" },
-});
+	{ timestamps: true }
+);
+
+module.export = mongoose.model(
+	"User",
+	UserSchema
+);
