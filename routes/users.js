a90225eb3ba3bfd102188b7783a830c471742c21
EhWanu <cameronjshadgett@gmail.com>
2021-10-29 22:08:12 -0400

    get user crud operation

--- routes/users.js
+++ routes/users.js
@@ -64,6 +64,17 @@ router.delete(
 );
 
 //GET
+
+router.get("/find/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const { password, ...info } = user._doc;
+		res.status(200).json(info);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 //GET ALL
 //GET USER STATS
 
